50b82bb478172306dbc6d01b0cda15f1e1ee9e54
F640 <92260436+F640@users.noreply.github.com>
2025-12-04 17:22:52 +0700

    removed kprobes requirement statement for in-kernel tracepoint support

--- kernel-scripts/set-features.sh
+++ kernel-scripts/set-features.sh
@@ -20,7 +20,7 @@ declare -a enable_feature_flags=(
     # "CONFIG_HAVE_KPROBES" # overidding this option and other CONFIG_HAVE_* options is somewhat not a good practice
     # same goes for this one
     # "CONFIG_HAVE_KRETPROBES" # not problematic, reason to disable this same as other CONFIG_HAVE_*
-    # Tracepoint for KernelSU and its forks just in case (needs kprobes)
+    # Tracepoint for KernelSU and its forks just in case
     # CONFIG_HAVE_SYSCALL_TRACEPOINTS # reason to disable this same as other CONFIG_HAVE_*
     "CONFIG_FTRACE"
     "CONFIG_TRACEPOINTS"
